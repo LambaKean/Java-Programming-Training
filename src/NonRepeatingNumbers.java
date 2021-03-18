941fe35333df01dfbdb2d792934b3edabf191b59
LambaKean <lambakean@gmail.com>
2021-03-18 16:08:29 +0300

    Various code style corrections and a new completed task

--- src/NonRepeatingNumbers.java
+++ src/NonRepeatingNumbers.java
@@ -1,10 +1,9 @@
-// Finding a second smallest element in an array (without taking into account the repetition of the minimum element)
-
+// Finding non-repeating values in an integer array
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Random;
 
-public class SecondSmallest {
+public class NonRepeatingNumbers {
 
 	public static void main (String[] args) {
 		
@@ -22,46 +21,50 @@ public class SecondSmallest {
 		
 		int[] array = new int[n];
 		
+		boolean[] flags = new boolean[n]; // true if i-th element of the array is repeating
+		
 		// Filling the array with random values ranging from lwrLimit to uprLimit
 		for (int i = 0; i < n; i++) {
 			
 			array[i] = random.nextInt(uprLimit + 1 - lwrLimit) + lwrLimit;
 			
+			flags[i] = false;
+			
 		}
 		
 		System.out.println("The generated array: " + Arrays.toString(array));
 		
-		
-		int firstMin = uprLimit;
-		// +1 to handle the situation when there're two same smallest values in the array
-		int secondMin = uprLimit + 1;
-		
 		for (int i = 0; i < n; i++) {
 			
-			if (array[i] < firstMin) {
-				
-				secondMin = firstMin;
-				
-				firstMin = array[i];
-				
-			} else if (array[i] > firstMin && array[i] < secondMin) {
-				
-				secondMin = array[i];
+			if(flags[i] == false) {
+			
+				for (int j = i+1; j < n; j++) {
+					
+					if(array[i] == array[j]) {
+						
+						flags[i] = true;
+						flags[j] = true;
+						
+					}
+					
+				}
 				
 			}
 			
 		}
 		
-		if (secondMin == uprLimit + 1) {
+		System.out.print("Non-repeating values in the array:");
+		
+		for (int i = 0; i < n; i++) {
 			
-			System.out.print("There's no a second smallest value in the array.");
-			
-		} else {
-			
-			System.out.print("A second smallest value in the array is: " + secondMin);
+			if (flags[i] == false) {
+				
+				System.out.print(" " + array[i]);
+				
+			}
 			
 		}
-	
+		
 		scanner.close();
 		
 	}
